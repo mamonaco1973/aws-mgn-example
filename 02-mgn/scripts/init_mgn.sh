56899a2937fa96d8e27108310e1ed9f53d72b69b
Mike Monaco <mamonaco1973@gmail.com>
2026-03-19 12:04:05 -0400

    work on script

--- 02-mgn/scripts/init_mgn.sh
+++ 02-mgn/scripts/init_mgn.sh
@@ -38,6 +38,7 @@ aws mgn create-replication-configuration-template \
   --replication-server-instance-type "${REPLICATION_INSTANCE_TYPE}" \
   --replication-servers-security-groups-ids "${TARGET_SECURITY_GROUP_ID}" \
   --staging-area-subnet-id "${STAGING_SUBNET_ID}" \
+  --staging-area-tags Name=mgn-staging,Project=mgn-lab \
   --use-dedicated-replication-server \
   --no-use-private-ip \
   >/tmp/mgn-replication-template.json 2>/tmp/mgn-replication-template.err || true
